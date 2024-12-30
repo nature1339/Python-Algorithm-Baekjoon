419166748d8905a321ee4c5fd363b933177d4f9e
nature1339 <76989460+nature1339@users.noreply.github.com>
2024-12-30 13:22:56 +0900

    추월

--- Python_Recent_Algorithm/20241221_silver02_2002_추월 연습.py
+++ Python_Recent_Algorithm/20241221_silver02_2002_추월 연습.py
@@ -10,9 +10,16 @@ overpass = set()
 car_in = [readl().rstrip(), for i in range(n)]  # a,b,c,d
 
 car_out = [readl().rstrip(), for i in range(n)] # a,b,c,d
-if car_in < car_out: #a < c
-    overpass.add(car_out)
-else:
+
+while i<=n or j<=n  :
+    if car_in[i] == car_out[j]:
+        car_in[i] += 1
+        car_out[j] += 1
+    elif car_in[i] < car_out[j]: #a < c
+        overpass.add(car_out)
+    else:
+        car_in[i] > car_out[j]:
+            
     
     
 
