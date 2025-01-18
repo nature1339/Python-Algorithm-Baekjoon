30d538ac9cdc54ecfae1cf3b5117d73a7dd6f67e
nature1339 <76989460+nature1339@users.noreply.github.com>
2025-01-18 15:03:19 +0900

    Update 20250111_silver03_11659_구간합구하기.py

--- Python_Recent_Algorithm/20250111_silver03_11659_구간합구하기.py
+++ Python_Recent_Algorithm/20250111_silver03_11659_구간합구하기.py
@@ -3,13 +3,23 @@ import sys
 sys.stdin = open("input_11659.txt")
 readl = sys.stdin.readline
 
-
 n, m = map(int, readl().split())  # 5  3
-arr = list(map(int, readl().rstrip()))  # 5 4 3 2 1
-pre = [0]
+# 5 4 3 2 1
+# arr = map(int, readl().split()) # error
+arr = list(map(int, readl().split()))  # 5 4 3 2 1
+pre = [0] # 0 5 9 12 14 15
 for x in arr:
     pre.append(pre[-1] + x)
+# [0]
+# [0 5]
+# [0 5 9]
+# ...
+# [0 5 9 12 14 15]
+#  0 1 2 3  4  5
 
 for _ in range(m):
+    # i번째, j번째
+    # 1 3
+    # pre[3] - pre[0]
     i, j = map(int, readl().split())
     print(pre[j] - pre[i - 1])
