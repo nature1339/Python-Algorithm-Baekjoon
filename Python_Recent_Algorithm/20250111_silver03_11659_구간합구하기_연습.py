cb11814acdd965077bcd50a883e5a151bffd5388
nature1339 <76989460+nature1339@users.noreply.github.com>
2025-01-21 18:42:37 +0900

    update

--- Python_Recent_Algorithm/20250111_silver03_11659_구간합구하기_연습.py
+++ Python_Recent_Algorithm/20250111_silver03_11659_구간합구하기_연습.py
@@ -5,9 +5,11 @@ readl = sys.stdin.readline
 
 n, m = map(int, readl().split())  # 5 4 3 2 1
 
-arr = list[readl().split()]
-s = [0]  # [0 5 9 12 14 15]
+arr = list(map(int, readl().split()))
+pre = [0]  # [0 5 9 12 14 15]
+for x in arr:
+    pre.append[pre[-1] + x]
 
-for i, j in range(m):  # 3 세번을 돌면서
-    m = readl().split()  # 불러옴 1 3
-    ans = s[j] - s[i]
+for _ in range(m):  # 3 세번을 돌면서
+    i, j = map(int, readl().split())  # 불러옴 1 3
+    print(pre[j] - pre[i - 1])
