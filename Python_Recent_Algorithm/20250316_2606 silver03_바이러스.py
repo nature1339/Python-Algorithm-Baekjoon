a3e96cfdd0b0d32b9ae24c682e3786827201ea93
sylee <nature1339@gmail.com>
2025-03-16 10:10:00 +0900

    2

---
+++ Python_Recent_Algorithm/20250316_2606 silver03_바이러스.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dq = deque([i for i in range(1, n + 1)])
+res = []
+
+while dq:
+    for i in range(k - 1):
+        dq.append(dq.popleft())
+    res.append(dq.popleft())
+
+print("<", ", ".join(list(map(str, res))), ">", sep="")
